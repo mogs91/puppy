65c7f81a9d6a09700e63722ba3b63e136ba00942
Mogs91 <markossayan@gmail.com>
2024-08-20 13:14:47 +0200

    importing the wolf package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mogs91/puppy
 
 go 1.23.0
+
+require github.com/mogs91/wolf v0.0.0-20240820111012-7326c5564a5b
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import(
+	"github.com/mogs91/wolf"
+)
+
 func Bark() string{
 	return "Woof!!"
 }
 
 func Barks()string{
 	return "Woof! Woof! Woof!"
+}
+
+
+func BigBark()string{
+	return wolf.WhenGrownUps(Bark())
 }
